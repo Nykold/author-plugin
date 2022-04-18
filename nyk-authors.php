65821c35870d3608abf85d469dad6c584cca5fb3
José Alves Amaro <jose.alves.amaro@gmail.com>
2022-04-18 14:08:03 +0100

    add comments for readibility

--- nyk-authors.php
+++ nyk-authors.php
@@ -3,16 +3,24 @@
 
     use Grav\Common\Plugin;
     use Grav\Plugin\Admin\Admin;
+    use Grav\Common\Page\Page;
+    use Grav\Common\Taxonomy;
 
     class NykAuthorsPlugin extends Plugin {
+
         public static function getSubscribedEvents() {
+
+            // The plugin will act whenever the page is saved, updating the author list if the taxonomy is changed
             return [
                 'onAdminSave' => ['addAuthorsNamesToPage', 0],
             ];
         }
 
         public function addAuthorsNamesToPage($event) {
+            // Set $page to the current page being edited
             $page = $event['object'];
+
+            // Apply only to pages under the template 'Blog Item' (blog_item.html.twig)
         }
     } 
 ?>
