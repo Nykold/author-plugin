3e2b49b67271f01a8252daf09f34c40d7e03bd88
José Alves Amaro <jose.alves.amaro@gmail.com>
2022-04-21 15:34:39 +0100

    Add new plugin initialization

--- nyk-authors.php
+++ nyk-authors.php
@@ -1,36 +1,74 @@
 <?php
     namespace Grav\Plugin;
 
+    use Composer\Autoload\ClassLoader;
     use Grav\Common\Plugin;
     use Grav\Plugin\Admin\Admin;
     use Grav\Common\Page\Page;
+    use Grav\Common\Page\Header;
+    use Grav\Common\Page\Interfaces\PageInterface;
     use Grav\Common\Taxonomy;
+    use RocketTheme\Toolbox\Event\Event;
 
+    /**
+     * Class NykAuthorsPlugin
+     * @package Grav\Plugin
+     */
     class NykAuthorsPlugin extends Plugin {
-
+        /**
+         * @return array
+         * 
+         * The getSubscribedEvents() gives the core a list of events
+         *     that the plugin wants to listen to. The key of each
+         *     array section is the event that the plugin listens to
+         *     and the value (in the form of an array) contains the
+         *     callable (or function) as well as the priority. The
+         *     higher the number the higher the priority.
+         */
         public static function getSubscribedEvents() {
-
-            // The plugin will act whenever the page is saved, updating the author list if the taxonomy is changed
             return [
-                'onAdminSave' => ['onAdminSave', 0],
+                'onPluginsInitialized' => ['onPluginsInitialized', 0],
             ];
         }
 
-        public function onAdminSave($event) {
+         /**
+         * Composer autoload.
+         * 
+         * @return ClassLoader
+         */
+        
+        public function autoload(): ClassLoader {
+            return require __DIR__ . '/vendor/autoload.php';
+        }
+
+        public function onPluginsInitialized() {
+            if ($this->isAdmin()) {
+                $this->enable([
+                    'onAdminSave' => ['onAdminSave', 0],
+                ]);
+                return;
+            }
+        }
+
+        public function onAdminSave(Event $event) {
             // Load plugin config into a variable
             $config = $this->config();
             $authorsConfig = $config['authors'];
 
             // Set $page to object being saved
             $page = $event['object'];
+            $header = $page->header();
+            if (!$header instanceof Header) {
+                $header = new Header((array)$header);
+            }
 
             // Only proceed if the saved object is a page
             if ($page instanceof Page) {
                 // Only proceed if page has template 'Blog Item' (blog_item.html.twig)
                 if ($page->name() === 'blog_item' . $page->extension()) {
                     // Check for authors in frontmatter
-                    if (isset($page->header()->taxonomy['author'])) {
-                        $pageAuthors = $page->header()->taxonomy['author'];
+                    if (isset($header->taxonomy['author'])) {
+                        $pageAuthors = $header->taxonomy['author'];
                     } else {
                         $pageAuthors = [];
                     }
@@ -60,7 +98,9 @@
 
                     // Write finalString into frontmatter
                     if ($finalString) {
-                        $page->header()->author_string = $finalString;
+                        $header->author_string = $finalString;
+                        $page->header() = $header;
+                        $event['object'] = $page;
                     }
                 }
             }
