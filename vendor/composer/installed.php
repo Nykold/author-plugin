849ef7a87f66876e864f1cae6857410729c8f6ef
José Alves Amaro <jose.alves.amaro@gmail.com>
2022-04-21 14:34:32 +0100

    Update to version 0.1.0 - Fix plugin blueprint - Add composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'grav-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '17927040/nyk-authors',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '17927040/nyk-authors' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'grav-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
