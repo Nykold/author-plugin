a1a8196c0412f44e3352fe70f16ab7848658d57c
José Alves Amaro <jose.alves.amaro@gmail.com>
2022-12-06 14:03:17 +0000

    Update composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'nykold/nyk-authors',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '02c5c789fd0bd9af50f097dc3353a6b551fcddec',
+        'reference' => 'e4e3f2ebc448e1445b68e2165683b7680c77858f',
         'type' => 'grav-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'nykold/nyk-authors' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '02c5c789fd0bd9af50f097dc3353a6b551fcddec',
+            'reference' => 'e4e3f2ebc448e1445b68e2165683b7680c77858f',
             'type' => 'grav-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
